066275d67d9808e9fee5f2a7406cbba56c40911d
sselvag <swaathy.selva@gmail.com>
2022-07-27 22:51:29 -0400

    text over hover and hide main

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $(".main").hide();
 
     var cityHistory = JSON.parse(localStorage.getItem("City History"));
     var city;
@@ -41,6 +42,8 @@ $(document).ready(function () {
     })
 
     function displayWeather() {
+        $(".main").show();
+
         var apiKey = "8c20fecf1dc12b4c826b47b8de6dbee6"
 
         var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + apiKey;
@@ -53,6 +56,7 @@ $(document).ready(function () {
             console.log(current);
 
             var name = current.name;
+            var conditions = current.weather[0].main
             var icon = current.weather[0].icon;
             var temperature = current.main.temp;
             var humidity = current.main.humidity;
@@ -60,7 +64,7 @@ $(document).ready(function () {
 
             $(".city").addClass("h3 px-2 pt-3").text(name);
             $(".date").addClass("h3 pt-3").text("(" + moment().format('L') + ")");
-            $(".icon").addClass("img-fluid").attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+            $(".icon").addClass("img-fluid").attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png").attr("title", conditions).attr("alt", conditions);
             $(".temperature").addClass("h5 p-2").text("Temperature: " + temperature + " °F");
             $(".humidity").addClass("h5 p-2").text("Humidity: " + humidity + "%");
             $(".wind-speed").addClass("h5 p-2").text("Wind Speed: " + windSpeed + " MPH");
@@ -96,6 +100,14 @@ $(document).ready(function () {
         }).then(function (forecast) {
             console.log("Current forecast URL is: " + forecastURL);
             console.log(forecast);
+
+            // <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+            //     <div class="card-body">
+            //         <h5 class="card-title">Primary card title</h5>
+            //         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            //     </div>
+            // </div>
+
         })
     }
 
